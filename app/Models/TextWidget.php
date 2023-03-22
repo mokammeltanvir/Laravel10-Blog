97b53326d9d90aea0126413ae9b08a9a180bb3c8
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-03-22 15:47:44 +0600

    create CRUD for TextWidget in admin panel

---
+++ app/Models/TextWidget.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class TextWidget extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'key',
+        'image',
+        'title',
+        'content',
+        'active',
+    ];
+
+    public static function getTitle(string $key): string
+    {
+        $widget = Cache::get('text-widget-'. $key, function() use ($key){
+            TextWidget::query()->where('key', '=', $key)->where('active', '=', true)->first();
+            });
+        if($widget){
+            return $widget->title;
+        }
+        return '';
+    }
+
+    public static function getContent(string $key): string
+    {
+        $widget = Cache::get('text-widget-'. $key, function() use ($key){
+            TextWidget::query()->where('key', '=', $key)->where('active', '=', true)->first();
+            });
+        if($widget){
+            return $widget->content;
+        }
+        return '';
+    }
+
+}
