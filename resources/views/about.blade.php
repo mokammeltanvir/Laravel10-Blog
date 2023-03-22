29a40b327ab0e6a5024aab3810d6ccd0335d4c22
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-03-22 23:32:18 +0600

    frontend about-us page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,46 @@
+<x-app-layout>
+  <!-- Post Section -->
+  <section class="w-full md:w-2/3 flex flex-col items-center px-3">
+
+    <article class="flex flex-col shadow my-4">
+      <!-- Article Image -->
+      <a href="{{ route('about-us') }}" class="hover:opacity-75">
+        <img src="/storage/{{ $widget->image }}">
+      </a>
+      <div class="bg-white flex flex-col justify-start p-6">
+        <h1 class="text-3xl font-bold hover:text-gray-700 pb-4">
+          {{ $widget->title }}</h1>
+        <div>
+          {!! $widget->content !!}
+        </div>
+      </div>
+    </article>
+
+    <div class="w-full flex flex-col text-center md:text-left md:flex-row shadow bg-white mt-10 mb-10 p-6">
+      <div class="w-full md:w-1/5 flex justify-center md:justify-start pb-4">
+        <img src="https://avatars.githubusercontent.com/u/69724418?v=4" class="rounded-full shadow h-32 w-32">
+      </div>
+      <div class="flex-1 flex flex-col justify-center md:justify-start">
+        <p class="font-semibold text-2xl">Mokammel Tanvir</p>
+        <p class="pt-2">Full-Stack Web Developer with over 5 years of experience in the industry.</p>
+        <div class="flex items-center justify-center md:justify-start text-2xl no-underline text-blue-800 pt-4">
+          <a class="" href="#">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a class="pl-4" href="#">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a class="pl-4" href="#">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a class="pl-4" href="#">
+            <i class="fab fa-linkedin"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+
+  </section>
+  <x-sidebar />
+
+</x-app-layout>
